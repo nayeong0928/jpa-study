b031a8fdb434566f6c2fca9031525a82e37400c9
yeong-coding <qoskdud0928@gmail.com>
2024-08-06 05:35:20 +0900

    객체를 테이블에 맞게 모델링한 경우

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -11,42 +11,15 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em=emf.createEntityManager();
 
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
         try{
-//            Member member1=new Member();
-//            Member member2=new Member();
-            Member member3=new Member();
 
-//            member1.setName("홍길동");
-//            member1.setRoleType(RoleType.USER);
-
-//            member2.setName("길동무");
-//            member2.setRoleType(RoleType.MANAGER);
-
-            member3.setName("이영순");
-            member3.setRoleType(RoleType.USER);
-
-//            System.out.println("=persist() 시작=");
-//            em.persist(member1);
-//            em.persist(member2);
-//            System.out.println("member1.getId() = " + member1.getId());
-//            System.out.println("member2.getId() = " + member2.getId());
-//            System.out.println("=persist() 끝");
-
-//            member1, member2를 insert하고 나서 실행하기
-            System.out.println("=persist(): member3 데이터 넣기");
-            em.persist(member3);
-            System.out.println("member3.getId() = " + member3.getId());
-            System.out.println("=persist() 끝=");
-//
-            System.out.println("=commit() 시작=");
-            transaction.commit();
-            System.out.println("=commit() 끝=");
+            tx.commit();
 
         } catch (Exception e){
-            transaction.rollback();
+            tx.rollback();
         } finally{
             em.close();
         }
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,46 +3,16 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@SequenceGenerator(name = "sequence_generator",
-sequenceName = "member_id_sequence",
-initialValue = 1,
-allocationSize = 40)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_generator")
+    @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
-    @Column
     private String name;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+    @Column(name = "team_id")
+    private Long teamId;
 
-    public Member() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
 }
