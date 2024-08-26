c30ff0e294db7fba88d075dbca4c9666616be73a
yeong-coding <qoskdud0928@gmail.com>
2024-08-27 08:16:11 +0900

    jpql 기본문법: 결과 조회

--- hello-jpa/src/main/java/jpql/JpaMain.java
+++ hello-jpa/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,7 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,6 +14,33 @@ public class JpaMain {
 
         try {
 
+            Member member=new Member();
+            member.setAge(30);
+            member.setUsername("홍길동");
+            em.persist(member);
+
+            // 리턴 타입 명확
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+
+            // 리턴 타입 명확X
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+            // 여러 개 결과 조회
+            List<Member> list = query1.getResultList();
+
+            for(Member m: list){
+                System.out.println("member: "+m.getUsername());
+            }
+
+            // 파라미터 바인딩
+            Member singleResult = em.createQuery("select m from Member m where m.username=:username", Member.class)
+                    .setParameter("username", "홍길동")
+                    .getSingleResult();
+
+            System.out.println("홍길동 멤버: "+singleResult.getId()+", "+singleResult.getAge());
+
+            tx.commit();
         }catch(Exception e){
             e.printStackTrace();
             tx.rollback();
